3eb39fca5e23c0c8586a12691ecd86cfb9220818
renkeles <84261162+renkeles@users.noreply.github.com>
2021-07-27 23:33:22 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,42 +1,154 @@
 #include <iostream>
 
-const int SIZE_ST = 1000;
+#define T int
 
-#define T char
-T Stack[SIZE_ST];
-int cursor = -1;
+typedef struct OneLinkNode{
+    T dat;
+    OneLinkNode *next;
+} OneLinkNode;
 
-bool push(T data){
-    if(cursor < SIZE_ST){
-        Stack[++cursor] = data;
-        return true;
-    }else{
-        std::cout << "(push) Stack overflow" << std::endl;
-        return false;
+typedef struct {
+    OneLinkNode *head;
+    int size;
+} OneLinkList;
+
+void initOneLinkList(OneLinkList *lst){
+    lst->head = nullptr;
+    lst->size = 0;
+}
+
+bool push(OneLinkList *stack, T value){
+    OneLinkNode *tmp = new OneLinkNode;
+    if(tmp == nullptr){
+        std::cout << "Stack overflow" << std::endl;
+        return 0;
+    }
+    tmp->dat = value;
+    tmp->next = stack->head;
+
+    stack->head = tmp;
+    stack->size++;
+}
+
+T pop(OneLinkList *stack){
+    if(stack->size == 0){
+        std::cout << "Stack is empty" << std::endl;
+        return -1;
+    }
+    OneLinkNode *tmp = stack->head;
+    T data = stack->head->dat;
+    stack->head = stack->head->next;
+    delete tmp;
+    stack->size--;
+    return data;
+
+}
+
+int bracesCheck(char* exp){
+    const int TYPES = 3;
+    char braces[][2] = {{'(', ')'}, {'{', '}'}, {'[', ']'}};
+    OneLinkList *st = new OneLinkList;
+    initOneLinkList(st);
+    int countIndex = 0;
+    while(*exp != '\0'){
+        for(int i = 0; i < TYPES; i++){
+            if(*exp == braces[i][0]){
+                push(st, i);
+            }
+        }
+        for(int i = 0; i < TYPES; i++){
+            if(*exp == braces[i][1]){
+                if(st->size == 0){
+                    return countIndex;
+                }
+                if(i == st->head->dat){
+                    pop(st);
+                }else{
+                    return countIndex;
+                }
+            }
+        }
+        exp++;
+        countIndex++;
     }
 }
 
-T pop(){
-    if(cursor != -1){
-        return Stack[cursor--];
-    }else{
-        std::cout << "(pop) Stack overflow" << std::endl;
-        return false;
+void printBC(int n){
+    std::cout << ((n == 0) ? "True" : "False") << std::endl;
+}
+
+void printOneLinkNode(OneLinkNode* node){
+    if(node == nullptr){
+        std::cout << "[]";
+        return;
     }
+    std::cout << "[" << node->dat << "]";
+}
+
+void printOneLinkList(OneLinkList* lst){
+    OneLinkNode* current = lst->head;
+    if(current == nullptr){
+        printOneLinkNode(current);
+    }else{
+        do{
+            printOneLinkNode(current);
+            current = current->next;
+        } while (current != nullptr);
+    }
+    std::cout << " Size: " << lst->size << std::endl;
+}
+
+void copyList(OneLinkList* from, OneLinkList* to){
+    int size = from->size;
+    OneLinkNode* current = from->head;
+    while(size != 0){
+        for(int i = 0; i < size - 1; i++){
+            current = current->next;
+        }
+        push(to, current->dat);
+        size--;
+        current = from->head;
+    }
+}
+
+bool isSorted(OneLinkList *lst){
+    OneLinkNode* current = lst->head;
+    while(current->next != nullptr){
+        if(current->dat > current->next->dat){
+            return 0;
+        }
+        current = current->next;
+    }
+    return 1;
 }
 
 int main() {
-    char str[] = "[2/{5*(4+7)}]";
 
-    int i = 0;
-    while(str[i] != '\0'){
-        if(str[i] == '('){
-            push(str[i]);
-            i++;
-        }else if(str[i] == ')' && cursor != -1){
-            pop();
-        }
+    printBC(bracesCheck("[]"));
+    printBC(bracesCheck("[]]"));
+    printBC(bracesCheck("{}(]"));
+    printBC(bracesCheck("{ ( [ {} [] () ] ( }"));
+    printBC(bracesCheck("[2/{5*(4+7)}]"));
+
+    OneLinkList* first = new OneLinkList;
+    initOneLinkList(first);
+    OneLinkList* second = new OneLinkList;
+    initOneLinkList(second);
+    for(int i = 0; i < 9; i++){
+        push(first, i);
     }
-    std::cout << (cursor == -1) ? 1:0;
+    printOneLinkList(first);
+    copyList(first, second);
+    printOneLinkList(second);
+
+    std::cout << ((isSorted(first)) ? "True" : "False") << std::endl;
+    OneLinkList* third = new OneLinkList;
+    initOneLinkList(third);
+    for(int i = 9; i > 0; i--){
+        push(third, i);
+    }
+    printOneLinkList(third);
+    std::cout << ((isSorted(third)) ? "True" : "False") << std::endl;
+
     return 0;
 }
